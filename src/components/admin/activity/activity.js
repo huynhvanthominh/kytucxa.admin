f4cfdd9f61a61fec8761e802377df3b5f810fc8a
Thơ Minh <thominh25111997@gmail.com>
2022-06-20 00:41:52 +0700

    update page activity

--- src/components/admin/activity/activity.js
+++ src/components/admin/activity/activity.js
@@ -1,18 +1,11 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import { Box } from "@mui/system";
 import { useEffect, useState } from "react";
-import PATH from "../../../consts/path";
-import { TOAST } from "../../../customs/toast-custom";
 import Table from "../../themes/table/table";
-import { roomAPI } from "../../../apis/room.api";
-import { materialService } from "../../../apis/material.api";
+import TableDateCustom from "../../../customs/Table-Date-custom";
 import { eventMaterialAPI } from "../../../apis/event-material.api"
 export default function Activity() {
     const title = "Hoạt động";
-    const [room, setRoom] = useState("");
     const [material, setMaterial] = useState("");
     const [materials, setMaterials] = useState([]);
-    const [rooms, setRooms] = useState([]);
     const [events, setEvents] = useState([]);
     const fetchEvents = async () => {
         try {
@@ -27,55 +20,6 @@ export default function Activity() {
     useEffect(() => {
         fetchEvents();
     }, [])
-    const fetchRooms = async () => {
-        try {
-            const { data } = await roomAPI.getRoomAdmin();
-            setRooms(data)
-        } catch (error) {
-            TOAST.EROR(error.message)
-        }
-    }
-
-    const fetchMaterials = async () => {
-        try {
-            const { data } = await materialService.get();
-            setMaterials(data)
-        } catch (error) {
-            TOAST.EROR(error.message)
-        }
-    }
-
-
-    const fetchData = () => {
-        fetchRooms();
-        fetchMaterials();
-    }
-
-    useEffect(() => {
-        fetchData()
-    }, [])
-
-    const Filter = () => {
-        return (
-            <Box>
-                <FormControl fullWidth size="small">
-                    <InputLabel>Loại hoạt động</InputLabel>
-                    <Select
-                        className="min-width-200"
-                        label="Phòng"
-                        value={material}
-                        onChange={e => setMaterial(e.target.value)}
-                    >
-                        <MenuItem value={-1}>Tất cả</MenuItem>
-                        {
-                            materials.map((material, i) => <MenuItem key={i} value={material?.id}>{material?.name}</MenuItem>)
-                        }
-                    </Select>
-                </FormControl>
-            </Box>
-        )
-    }
-
 
     return (
         <div>
@@ -86,21 +30,39 @@ export default function Activity() {
             </div>
             <div className="border-bottom border-primary border-5" />
             <div className="py-4">
-                <Table dataSource={events} hover striped border filter={<Filter />}>
+                <Table dataSource={events} hover striped border>
                     {{
                         columns: [
                             {
-                                title: "",
-                                search: false,
-                                data: "nameEventMaterial",
+                                title: "Tên hoạt động",
+                                search: true,
+                                data: "nameEvent",
                                 className: "justify-content-center",
-                                render: (data) => <div className="table-img"><img src={PATH.MATERIAL + data} alt="" /></div>
                             },
                             {
-                                title: "Tinh trạng",
-                                data: "nameEventMaterial",
+                                title: "Vật chất",
+                                search: true,
+                                data: "nameMaterial",
                                 className: "justify-content-center",
-                                sort: true,
+                            },
+                            {
+                                title: "Từ",
+                                search: true,
+                                data: "roomFrom",
+                                className: "justify-content-center",
+                            },
+                            {
+                                title: "Đến",
+                                search: true,
+                                data: "roomTo",
+                                className: "justify-content-center",
+                            },
+                            {
+                                title: "Thời gian",
+                                search: true,
+                                data: "createdAt",
+                                className: "justify-content-center",
+                                render: (data) => <TableDateCustom date={data}/>
                             },
                         ],
                     }}
